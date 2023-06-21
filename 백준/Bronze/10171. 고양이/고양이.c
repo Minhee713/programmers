f6ecfcfa08ea1b20d6303013c15ddfead958042a
min-hee Kwon <127821647+Minhee713@users.noreply.github.com>
2023-06-21 13:59:19 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+        printf("\\    \/\\\n");
+        printf(" \)  \( \'\)\n");
+        printf("\(  \/  \)\n");
+        printf(" \\\(\_\_\)\|\n");
+
+        return 0;
+}
